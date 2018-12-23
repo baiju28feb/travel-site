3d3124b64aef2961bd84492418adfe00cb79eefc
baiju <baiju28feb@gmail.com>
2018-12-23 12:16:26 +0530

    working till gulp and postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
-
-gulp.task('default',function(){
-    console.log("horaay  gulp task")
-});
-
-gulp.task('html',function(){
-    console.log("abc");
-});
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
 gulp.task('styles',function(){
-    gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest(''))
-})
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
         gulp.start('html');
@@ -19,5 +18,5 @@ gulp.task('watch', function(){
 });
 
 watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
-})
+     gulp.start('styles');
+});
